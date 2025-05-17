f2517fa40a8e5665d162d2639ff87fa015ea3ac9
김병준 <kmbzn24@hanyang.ac.kr>
2025-05-17 10:34:49 +0900

    margin

--- docs/.vuepress/.temp/internal/routes.js
+++ docs/.vuepress/.temp/internal/routes.js
@@ -2,6 +2,7 @@ export const redirects = JSON.parse("{}")
 
 export const routes = Object.fromEntries([
   ["/", { loader: () => import(/* webpackChunkName: "index.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"kmbzn.com"} }],
+  ["/finance/%EB%82%98%EB%8A%94-%EC%99%9C-%EB%B9%84%ED%8A%B8%EC%BD%94%EC%9D%B8%EC%97%90-%ED%88%AC%EC%9E%90%ED%96%88%EB%8A%94%EA%B0%80.html", { loader: () => import(/* webpackChunkName: "finance_나는-왜-비트코인에-투자했는가.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/finance/나는-왜-비트코인에-투자했는가.html.js"), meta: {"title":"나는 왜 비트코인에 투자했는가"} }],
   ["/computer-graphics/1-Course-Intro.html", { loader: () => import(/* webpackChunkName: "computer-graphics_1-Course-Intro.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/computer-graphics/1-Course-Intro.html.js"), meta: {"title":"1 - Course Intro"} }],
   ["/computer-graphics/2-Rendering-Basics.html", { loader: () => import(/* webpackChunkName: "computer-graphics_2-Rendering-Basics.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/computer-graphics/2-Rendering-Basics.html.js"), meta: {"title":"2 - Rendering Basics"} }],
   ["/computer-graphics/3-Transformations.html", { loader: () => import(/* webpackChunkName: "computer-graphics_3-Transformations.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/computer-graphics/3-Transformations.html.js"), meta: {"title":"3 - Transformations"} }],
@@ -11,7 +12,6 @@ export const routes = Object.fromEntries([
   ["/computer-graphics/7-Hierachical-Modeling-Mesh.html", { loader: () => import(/* webpackChunkName: "computer-graphics_7-Hierachical-Modeling-Mesh.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/computer-graphics/7-Hierachical-Modeling-Mesh.html.js"), meta: {"title":"7 - Hierarchical Modeling, Mesh"} }],
   ["/computer-graphics/8-Lighting.html", { loader: () => import(/* webpackChunkName: "computer-graphics_8-Lighting.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/computer-graphics/8-Lighting.html.js"), meta: {"title":"8 - Lighting"} }],
   ["/computer-graphics/Project-2.html", { loader: () => import(/* webpackChunkName: "computer-graphics_Project-2.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/computer-graphics/Project-2.html.js"), meta: {"title":"Computer Graphics Project 2: Obj viewer"} }],
-  ["/finance/%EB%82%98%EB%8A%94-%EC%99%9C-%EB%B9%84%ED%8A%B8%EC%BD%94%EC%9D%B8%EC%97%90-%ED%88%AC%EC%9E%90%ED%96%88%EB%8A%94%EA%B0%80.html", { loader: () => import(/* webpackChunkName: "finance_나는-왜-비트코인에-투자했는가.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/finance/나는-왜-비트코인에-투자했는가.html.js"), meta: {"title":"나는 왜 비트코인에 투자했는가"} }],
   ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"/home/kmbzn/workspace/blog-v2/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
 ]);
 
