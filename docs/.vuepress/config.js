829311c972f55e5a1066e65f419180407b981828
김병준 <kmbzn24@hanyang.ac.kr>
2025-06-02 11:02:07 +0900

    vim

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -35,7 +35,8 @@ export default defineUserConfig({
         collapsible: false,
         children: [
           '/Mindscape',
-          '/Musics'
+          '/Musics',
+          '/vim'
         ]
       },
       {
